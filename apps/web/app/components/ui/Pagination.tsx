7bfb62f82e536a11dbc671a3896ce88d82bd3b14
unknown <me@here.com>
2024-07-16 15:43:27 +0700

    fix deploy

--- apps/web/app/components/ui/Pagination.tsx
+++ apps/web/app/components/ui/Pagination.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Pagination = () => {
-  return (
-    <>
-      <Pagination total={10} />
-    </>
-  );
+  return <>{/* <Pagination total={10} /> */}</>;
 };
 
 export default Pagination;
