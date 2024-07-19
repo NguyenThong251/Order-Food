d94f871cb14c6e1869f122a69ec09bc1bccacaf5
unknown <me@here.com>
2024-07-19 11:05:47 +0700

    search

---
+++ apps/web/app/interface/table.interface.tsx
@@ -0,0 +1,4 @@
+export interface table {
+  _id: string;
+  name: string;
+}
--- apps/web/app/store/user/user.store.ts
+++ apps/web/app/store/user/user.store.ts
@@ -1,19 +1,23 @@
-// import { persist, create, devtools } from "@repo/ui";
-// import { User, Action } from "../../interface";
+import { persist, create, devtools } from "@repo/ui";
+import { User, Action } from "../../interface";
 
-// export const useUserStore = create<User & Action>()(
-//   devtools(
-//     persist(
-//       (set) => ({
-//         name: "",
-//         phone: 0,
-//         setUser: (user: User) => set(user),
-//         clearUser: () => set({ name: "", phone: 0 }),
-//       }),
-//       {
-//         name: "user-session",
-//         getStorage: () => sessionStorage,
-//       }
-//     )
-//   )
-// );
+export const useUserStore = create<Action>()(
+  devtools(
+    persist(
+      (set) => ({
+        // username: "",
+        // phone: 0,
+        // isAdmin: false,
+        // email:"",
+        // password: "",
+
+        setUser: (user: User) => set(user),
+        clearUser: () => set({ name: "", phone: 0 }),
+      }),
+      {
+        name: "user-session",
+        getStorage: () => sessionStorage,
+      }
+    )
+  )
+);
